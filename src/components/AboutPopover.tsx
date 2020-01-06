adc4eb76df49f31171787dc31d509599a8d2ec8e
Arnav Meduri <arnav.meduri@gmail.com>
2020-01-06 15:47:04 -0500

    removed menues from About page that are not needed

--- src/components/AboutPopover.tsx
+++ src/components/AboutPopover.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import { IonList, IonItem, IonLabel } from '@ionic/react';
+import React from "react";
+import { IonList, IonItem, IonLabel } from "@ionic/react";
 
 interface AboutPopoverProps {
   dismiss: () => void;
-};
+}
 
-const AboutPopover: React.FC<AboutPopoverProps> = ({dismiss}) => {
-
+const AboutPopover: React.FC<AboutPopoverProps> = ({ dismiss }) => {
   const close = (url: string) => {
-    window.open(url, '_blank');
+    window.open(url, "_blank");
     dismiss();
   };
 
   return (
     <IonList>
-      <IonItem button onClick={() => close('https://ionicframework.com/getting-started')}>
+      {/* <IonItem button onClick={() => close('https://ionicframework.com/getting-started')}>
         <IonLabel>Learn Ionic</IonLabel>
       </IonItem>
       <IonItem button onClick={() => close('https://ionicframework.com/docs/react')}>
@@ -22,15 +21,18 @@ const AboutPopover: React.FC<AboutPopoverProps> = ({dismiss}) => {
       </IonItem>
       <IonItem button onClick={() => close('https://showcase.ionicframework.com')}>
         <IonLabel>Showcase</IonLabel>
-      </IonItem>
-      <IonItem button onClick={() => close('https://github.com/ionic-team/ionic')}>
+      </IonItem> */}
+      <IonItem
+        button
+        onClick={() => close("https://github.com/arnav-meduri/fetch")}
+      >
         <IonLabel>GitHub Repo</IonLabel>
       </IonItem>
-      <IonItem button onClick={dismiss}>
+      {/* <IonItem button onClick={dismiss}>
         <IonLabel>Support</IonLabel>
-      </IonItem>
-    </IonList >
-  )
-}
+      </IonItem> */}
+    </IonList>
+  );
+};
 
 export default AboutPopover;
